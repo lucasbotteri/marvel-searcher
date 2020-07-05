085d6a25f992006b743f4879c8f8bdf34ee95507
lucas <lucasbotteri@hotmail.com>
2020-07-04 23:41:02 -0300

    Added Card Component (#5)
    Created Card Component
    Added fontawesome icons and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card
+        name="spiderman"
+        onClick={() => {}}
+        imageSource="https://i.pinimg.com/originals/32/92/5e/32925e3718ee000e49c79f06f6c4389e.jpg"
+      />
+    </>
   );
 }
 
