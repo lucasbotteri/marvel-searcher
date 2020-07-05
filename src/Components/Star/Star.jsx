32dce4ec109fd3e11a0216d51f7e69cf69577b9c
lucas <lucasbotteri@hotmail.com>
2020-07-05 01:53:33 -0300

    Feat: Added star component (#7)
    Extracted star solid vs regular logic to star component
    Added regular svg icons

---
+++ src/Components/Star/Star.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { faStar as SolidStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as RegularStar } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StarIcon = styled(FontAwesomeIcon)`
+  color: ${(props) => props.theme.primaryColor};
+  cursor: pointer;
+`;
+
+const Star = ({ onClick, isSolid, className }) => {
+  const svgStar = isSolid ? SolidStar : RegularStar;
+  return <StarIcon className={className} icon={svgStar} onClick={onClick} />;
+};
+
+Star.propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  isSolid: PropTypes.bool,
+};
+Star.defaultProps = {
+  className: "",
+  isSolid: false,
+};
+
+export default Star;
