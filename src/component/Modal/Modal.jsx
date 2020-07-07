8f8dc26ad59876da01ec5b907f2fcde7afb4a74b
Lucas Botteri <lucasbotteri@hotmail.com>
2020-07-06 21:36:25 -0300

    Added modal component
    * Added font styles colors
    * Added responsive modal

---
+++ src/component/Modal/Modal.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import { faTimes as CloseSvg } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+
+const ModalBackground = styled.div`
+  background-color: ${(props) => props.theme.opaqueColor};
+  height: 100%;
+  left: 0;
+  overflow: auto;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+`;
+
+const ModalWrapper = styled.aside`
+  background-color: ${(props) => props.theme.primaryColor};
+  border-radius: ${(props) => props.theme.radius};
+  border: 1px solid ${(props) => props.theme.secondaryColor};
+  box-sizing: content-box;
+  display: flex;
+  flex-direction: column;
+  font-size: 19px;
+  margin: 10% auto;
+  padding: 10px;
+  width: 450px;
+
+  @media (max-width: 490px) {
+    height: 90%;
+    margin-top: 10%;
+    margin: 0;
+    width: 95%;
+  }
+`;
+
+const ModalContent = styled.div`
+  align-self: center;
+  padding: 10px;
+`;
+
+const CloseIcon = styled(FontAwesomeIcon)`
+  align-self: flex-end;
+  color: ${(props) => props.theme.secondaryColor};
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.opaqueColor};
+  }
+`;
+
+const ModalTitle = styled.h3`
+  align-self: flex-start;
+  color: ${(props) => props.theme.primaryFontColor};
+  font-family: ${(props) => props.theme.modalFont};
+  font-weight: bold;
+`;
+
+const Modal = ({ children, title, isShowing, handleClose }) => {
+  return isShowing ? (
+    <ModalBackground>
+      <ModalWrapper role="dialog" aria-labelledby="modal-title">
+        <CloseIcon
+          icon={CloseSvg}
+          onClick={handleClose}
+          aria-label="close modal"
+        />
+        <ModalTitle id="modal-title"> {title}</ModalTitle>
+        <ModalContent>{children}</ModalContent>
+      </ModalWrapper>
+    </ModalBackground>
+  ) : null;
+};
+
+Modal.propTypes = {
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string.isRequired,
+  isShowing: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
--- src/theme/default.js
+++ src/theme/default.js
@@ -1,6 +1,11 @@
 export default {
-  primaryColor: "#ffffff",
-  secondaryColor: "#A8A8A880",
   appBackgroundColor: "#F0F0F0",
+  modalFont: "Monserrat",
+  opaqueColor: "#00000076",
+  primaryColor: "#ffffff",
+  primaryFontColor: "#4D4D4D",
   radius: "7px",
+  secondaryColor: "#A8A8A880",
+  secondaryFontColor: "#515151",
+  semiBold: "500",
 };
