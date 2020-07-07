dbe2ec56828de55a4833072424cd2b061ac20739
Lucas Botteri <lucasbotteri@hotmail.com>
2020-07-07 20:44:53 -0300

    Added comic detail component

---
+++ src/component/ComicDetail/ComicDetail.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const ComicDetailWrapper = styled.div`
+  align-items: start;
+  display: grid;
+  grid-template-areas:
+    "image title"
+    "image creators"
+    "image description"
+    "image .";
+  grid-template-columns: 50% auto;
+  grid-template-rows: repeat(4, auto);
+  margin: auto;
+  width: 66%;
+`;
+
+const ComicImage = styled.img`
+  grid-area: image;
+`;
+
+const ComicTitle = styled.h2`
+  font-size: ${(props) => props.theme.h2Size};
+  grid-area: title;
+`;
+
+const ComicCreator = styled.p`
+  font-size: 12px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const ComicCreatorsWrapper = styled.div`
+  grid-area: creators;
+`;
+
+const ComicDescription = styled.p`
+  font-size: 11px;
+  grid-area: description;
+`;
+
+const ComicDetail = ({ comic }) => {
+  return (
+    <ComicDetailWrapper>
+      <ComicImage
+        src={comic.thumbnail}
+        alt={`Comic ${comic.title} thumbnail`}
+      />
+      <ComicTitle>{comic.title}</ComicTitle>
+      <ComicCreatorsWrapper>
+        <ComicCreator>{`Published: ${comic.published}`}</ComicCreator>
+        <ComicCreator>{`Writer: ${comic.writer}`}</ComicCreator>
+        <ComicCreator>{`Penciler: ${comic.penciler}`}</ComicCreator>
+        <ComicCreator>{`Cover Artist: ${comic.cover}`}</ComicCreator>
+      </ComicCreatorsWrapper>
+      <ComicDescription>{comic.description}</ComicDescription>
+    </ComicDetailWrapper>
+  );
+};
+
+ComicDetail.propTypes = {
+  comic: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    thumbnail: PropTypes.string,
+    description: PropTypes.string,
+    writer: PropTypes.string,
+    penciler: PropTypes.string,
+    cover: PropTypes.string,
+    published: PropTypes.string,
+  }).isRequired,
+};
+
+export default ComicDetail;
